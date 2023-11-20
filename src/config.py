686ee704de1b12efc0564edc02621e01466c37cd
Kulakshi Fernando <kulakshif@gmail.com>
2023-11-20 12:33:47 +0100

    updated mongodb connection

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
-MONGO_URI = "mongodb://localhost:27017/"
-DATABASE_NAME = "fastapi_storage"
+MONGO_URI = "mongodb+srv://dlog:dlogdb900@cluster0.wdwpxzz.mongodb.net/?retryWrites=true&w=majority"
+# MONGO_URI = "mongodb://localhost:27017/"
+DATABASE_NAME = "dlog"
 COLLECTION_NAME = "elements"
