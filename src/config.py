9ee04860528432d9593fda1f1fb48bf5f9e8cdbb
Kulakshi Fernando <kulakshif@gmail.com>
2024-01-23 03:23:26 +0100

    missed files

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-# MONGO_URI = "mongodb+srv://dlog:dlogdb900@cluster0.wdwpxzz.mongodb.net/?retryWrites=true&w=majority"
-MONGO_URI = "mongodb://localhost:27017/"
+MONGO_URI = "mongodb+srv://dlog:dlogdb900@cluster0.wdwpxzz.mongodb.net/?retryWrites=true&w=majority"
+# MONGO_URI = "mongodb://localhost:27017/"
 DATABASE_NAME = "dlog"
 COLLECTION_NAME = "elements"
